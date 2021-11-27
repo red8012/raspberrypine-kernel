58d3a8721c4a615cc65d0c178595b4968b36eba8
Yu-Ann Chen <red8012@gmail.com>
2021-11-27 10:31:58 +0800

    debug

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -xeuo pipefail
 
+ls
 mv APKBUILD config-changes-rpi4.aarch64 /home/builder
 apk add alpine-sdk sudo
 adduser -G abuild -g "Alpine Package Builder" -s /bin/sh -D builder
