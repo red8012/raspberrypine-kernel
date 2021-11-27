37959ddd699f2eb3215d023499a254d704d34221
Yu-Ann Chen <red8012@gmail.com>
2021-11-27 11:33:22 +0800

    get rid of doas

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 apk add alpine-sdk sudo doas
 adduser -G abuild -g "Alpine Package Builder" -s /bin/sh -D builder
 echo "builder ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-doas addgroup builder abuild
+addgroup builder abuild
 mv builder.sh APKBUILD config-changes-rpi4.aarch64 /home/builder
 cd /home/builder
 su -c 'sh builder.sh' builder
