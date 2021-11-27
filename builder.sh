46b1aea5e485bcdb9e3a7a5b8dcdd4b2ae5a33a6
Yu-Ann Chen <red8012@gmail.com>
2021-11-27 11:47:20 +0800

    sudo

--- builder.sh
+++ builder.sh
@@ -7,7 +7,7 @@ git clone --depth 1 https://gitlab.alpinelinux.org/alpine/aports
 mkdir -p ~/aports/testing/raspberrypine-kernel
 sudo mv /builder.sh /APKBUILD /config-changes-rpi4.aarch64 ~/aports/testing/raspberrypine-kernel
 cd ~/aports/testing/raspberrypine-kernel
-chmod 777 *
+sudo chmod 777 *
 
 abuild-keygen -a -i -n
 abuild checksum
