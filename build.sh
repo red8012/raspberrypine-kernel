0243f82a12175e3dd2768c45f8a44a643afb7bb3
Yu-Ann Chen <red8012@gmail.com>
2021-11-27 10:34:29 +0800

    fix order

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -xeuo pipefail
 
 ls
-mv APKBUILD config-changes-rpi4.aarch64 /home/builder
 apk add alpine-sdk sudo
 adduser -G abuild -g "Alpine Package Builder" -s /bin/sh -D builder
 echo "builder ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+mv APKBUILD config-changes-rpi4.aarch64 /home/builder
 su builder
 cd /home/builder
 ls
