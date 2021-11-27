d67983d94f27db4dccdb2c33e839294d1e6ae824
Yu-Ann Chen <red8012@gmail.com>
2021-11-27 11:43:54 +0800

    remove cd

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ apk add alpine-sdk sudo doas
 adduser -G abuild -g "Alpine Package Builder" -s /bin/sh -D builder
 echo "builder ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 addgroup builder abuild
-cd /home/builder
 su -c 'sh builder.sh' builder
