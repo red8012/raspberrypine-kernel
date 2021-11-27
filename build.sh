700a7b200c18889b732772585e6dad19b70a1d22
Yu-Ann Chen <red8012@gmail.com>
2021-11-27 11:30:17 +0800

    fix doas

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -xeuo pipefail
 apk add alpine-sdk sudo doas
 adduser -G abuild -g "Alpine Package Builder" -s /bin/sh -D builder
 echo "builder ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+doas addgroup builder abuild
 mv builder.sh APKBUILD config-changes-rpi4.aarch64 /home/builder
 cd /home/builder
 su -c 'sh builder.sh' builder
--- builder.sh
+++ builder.sh
@@ -7,7 +7,6 @@ mkdir -p main/linux-rpi
 mv APKBUILD config-changes-rpi4.aarch64 main/linux-rpi
 cd main/linux-rpi
 
-doas addgroup builder abuild
 abuild-keygen -a -i -n
 abuild checksum
 abuild -r
