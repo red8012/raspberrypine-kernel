030f742424394173778fd46dc4235e73e0df98c4
Yu-Ann Chen <red8012@gmail.com>
2021-11-27 10:38:02 +0800

    abuild

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -xeuo pipefail
 
-ls
 apk add alpine-sdk sudo
 adduser -G abuild -g "Alpine Package Builder" -s /bin/sh -D builder
 echo "builder ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
@@ -9,3 +8,5 @@ mv APKBUILD config-changes-rpi4.aarch64 /home/builder
 su builder
 cd /home/builder
 ls
+
+abuild
