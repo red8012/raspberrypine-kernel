95321b32b40b81f72f62fd4534eaf9b364360ff8
Yu-Ann Chen <red8012@gmail.com>
2021-11-27 11:17:58 +0800

    permission?

--- builder.sh
+++ builder.sh
@@ -3,6 +3,10 @@ set -xeuo pipefail
 
 cd ~
 pwd
+mkdir -p main/linux-rpi
+mv APKBUILD config-changes-rpi4.aarch64 main/linux-rpi
+cd main/linux-rpi
+
 abuild-keygen -a -i -n
 abuild checksum
 abuild -r
