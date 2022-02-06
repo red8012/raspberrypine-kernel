344f1eef21a2b1c39ec8bf0a4262b6cb4329447f
Yu-Ann Chen <red8012@gmail.com>
2022-02-06 15:31:31 +0800

    build-dep?

--- prepare.sh
+++ prepare.sh
@@ -6,6 +6,7 @@ cd ~
 apt update
 apt install -y dpkg-dev eatmydata
 apt install -y fakeroot git kernel-wedge quilt ccache flex bison libssl-dev dh-exec rsync libelf-dev bc crossbuild-essential-arm64
+apt install -y libssl-dev libelf-dev gcc-11 gcc-arm-linux-gnueabihf zlib1g-dev libcap-dev libpci-dev libglib2.0-dev libudev-dev libwrap0-dev libaudit-dev libbabeltrace-dev libdw-dev libiberty-dev libnewt-dev libnuma-dev libperl-dev libunwind-dev libopencsd-dev python3-dev dvipng
 
 apt-get build-dep -y linux-image-$LINUX_VER
 apt-get source -y linux-image-$LINUX_VER
