6fcd3539e1db6d9df19a925b8ddc3f506d7af896
Yu-Ann Chen <red8012@gmail.com>
2022-02-06 20:17:43 +0800

    fix

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,7 @@ cd ~
 dpkg --add-architecture arm64
 apt update
 apt install -y dpkg-dev eatmydata crossbuild-essential-arm64 build-essential
-apt install fakeroot git kernel-wedge quilt ccache flex bison libssl-dev dh-exec rsync libelf-dev bc
+apt install -y fakeroot git kernel-wedge quilt ccache flex bison libssl-dev dh-exec rsync libelf-dev bc
 
 # apt-get build-dep -y linux-image-$LINUX_VER
 apt-get source -y linux-image-$LINUX_VER
