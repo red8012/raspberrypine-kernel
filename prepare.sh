7ed32bc6168c14de7784aed3978e7352e3387d29
Yu-Ann Chen <red8012@gmail.com>
2022-02-06 15:03:49 +0800

    deb-src

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,11 @@
 set -xeuo pipefail
 
+echo "deb-src http://deb.debian.org/debian bookworm main" > /etc/apt/sources.list
 cat /etc/apt/sources.list
-# apt update
-# apt install -y dpkg-dev eatmydata
-# apt-get build-dep linux-image-$LINUX_VER
-# apt-get source linux-source-$LINUX_VER
+apt update
+apt install -y dpkg-dev eatmydata
+apt-get build-dep linux-image-$LINUX_VER
+apt-get source linux-source-$LINUX_VER
 
 
 # apt install build-essential bc kmod cpio flex libncurses5-dev libelf-dev libssl-dev dwarves
