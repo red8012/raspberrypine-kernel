0d466374582a219fd93e7e7ee2e75c59ffabd7c7
Yu-Ann Chen <red8012@gmail.com>
2022-02-06 18:49:10 +0800

    trial

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 set -xeuo pipefail
 
 cd ~/linux-*
-export DEB_BUILD_PROFILES="cross nopython nodoc pkg.linux.notools"
-dpkg-buildpackage -uc -b -a arm64
+dpkg-buildpackage -uc -b -a arm64 -P=cross,nopython,nodoc,pkg.linux.notools
