22a2e6314bb8fc485986406615608176d13ef456
Yu-Ann Chen <red8012@gmail.com>
2022-02-07 07:18:09 +0800

    no debug

--- build.sh
+++ build.sh
@@ -10,9 +10,10 @@ export $(dpkg-architecture -a$ARCH)
 export PATH=/usr/lib/ccache:$PATH
 export DEB_BUILD_PROFILES="cross nopython nodoc pkg.linux.notools"
 export MAKEFLAGS="-j$(($(nproc)*1))"
-export DEBIAN_KERNEL_DISABLE_DEBUG=
+export DEBIAN_KERNEL_DISABLE_DEBUG=yes
 fakeroot make -f debian/rules clean
 fakeroot make -f debian/rules orig
 fakeroot make -f debian/rules source
+echo "$(dpkg-parsechangelog --show-field Distribution)"
 fakeroot make -f debian/rules.gen setup_${ARCH}_${FEATURESET}_${FLAVOUR}
 fakeroot make -f debian/rules.gen binary-arch_${ARCH}_${FEATURESET}_${FLAVOUR}
