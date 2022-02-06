6e4b22e58ea9e51323ff74bf5450ad4b47e79a01
Yu-Ann Chen <red8012@gmail.com>
2022-02-06 15:25:22 +0800

    cross compile

--- prepare.sh
+++ prepare.sh
@@ -2,8 +2,11 @@ set -xeuo pipefail
 
 echo "deb-src http://deb.debian.org/debian bookworm main" >> /etc/apt/sources.list
 cd ~
+
 apt update
 apt install -y dpkg-dev eatmydata
+apt install -y fakeroot git kernel-wedge quilt ccache flex bison libssl-dev dh-exec rsync libelf-dev bc crossbuild-essential-arm64
+
 apt-get build-dep -y linux-image-$LINUX_VER
 apt-get source -y linux-image-$LINUX_VER
 
