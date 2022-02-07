2da50967cecc9c8d36c20245d10510b3721eab08
Yu-Ann Chen <red8012@gmail.com>
2022-02-07 12:27:25 +0800

    j8; deb_artifact

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ FLAVOUR=cloud-arm64
 export $(dpkg-architecture -a$ARCH)
 export PATH=/usr/lib/ccache:$PATH
 export DEB_BUILD_PROFILES="cross nopython nodoc pkg.linux.notools"
-export MAKEFLAGS="-j$(($(nproc)*1))"
+export MAKEFLAGS="-j8"
 export DEBIAN_KERNEL_DISABLE_DEBUG=yes
 time fakeroot make -f debian/rules clean
 time fakeroot make -f debian/rules orig
@@ -17,3 +17,7 @@ time fakeroot make -f debian/rules source
 echo "$(dpkg-parsechangelog --show-field Distribution)"
 time fakeroot make -f debian/rules.gen setup_${ARCH}_${FEATURESET}_${FLAVOUR}
 time fakeroot make -f debian/rules.gen binary-arch_${ARCH}_${FEATURESET}_${FLAVOUR}
+
+mkdir ~/deb_artifacts
+mv *.deb ~/deb_artifacts
+ls ~/deb_artifacts
