fac9c96ffba2442457a1e92bbbc636119016d3fc
Yu-Ann Chen <red8012@gmail.com>
2022-02-11 22:03:41 +0800

    test

--- build.sh
+++ build.sh
@@ -16,8 +16,11 @@ echo "$(dpkg-parsechangelog --show-field Distribution)"
 time fakeroot make -f debian/rules.gen setup_${ARCH}_${FEATURESET}_${FLAVOUR}
 # time eatmydata fakeroot make -f debian/rules.gen binary-arch_${ARCH}_${FEATURESET}_${FLAVOUR}
 ls -a
+ls -a debian/
+ls -a debian/build/
+ls -a debian/build/build_arm64_none_cloud-arm64
 
 mkdir ~/deb_artifacts
-mv ../.config ~/deb_artifacts
+mv *.config ~/deb_artifacts
 mv ../*.deb ~/deb_artifacts
 ls ~/deb_artifacts
